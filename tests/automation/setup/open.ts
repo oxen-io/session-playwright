6e8cdf77d7c0959e2ac1c19e0b0cf30580936d73
Audric Ackermann <audric@loki.network>
2024-03-18 16:01:51 +1100

    feat: allow to use session-desktop bins to start tests

--- tests/automation/setup/open.ts
+++ tests/automation/setup/open.ts
@@ -1,6 +1,6 @@
-import { join } from 'path';
-import { isEmpty } from 'lodash';
 import { _electron as electron } from '@playwright/test';
+import { isEmpty } from 'lodash';
+import { join } from 'path';
 
 export const NODE_ENV = 'production';
 export const MULTI_PREFIX = 'test-integration-testnet-';
@@ -13,6 +13,15 @@ function getAppRootPath() {
     );
   }
   return process.env.SESSION_DESKTOP_ROOT as string;
+}
+
+function getSessionDesktopBinPath() {
+  if (isEmpty(process.env.SESSION_DESKTOP_BIN)) {
+    throw new Error(
+      "You need to set the 'SESSION_DESKTOP_BIN' env variable to the session-desktop bin you want to test first (maybe `/usr/bin/session-desktop` ?)",
+    );
+  }
+  return process.env.SESSION_DESKTOP_BIN as string;
 }
 
 export async function openApp(windowsToCreate: number) {
@@ -39,8 +48,24 @@ const openElectronAppOnly = async (multi: string) => {
   }`;
   process.env.NODE_ENV = NODE_ENV;
 
+  if (!isEmpty(process.env.CI)) {
+    const sessionBinPath = getSessionDesktopBinPath();
+    const fakeHome = `/tmp/${process.env.NODE_APP_INSTANCE}`;
+
+    console.info(`   CI RUN`);
+    console.info(`   SESSION_BIN_PATH=${sessionBinPath}`);
+    console.info(`   HOME="${fakeHome}"`);
+
+    process.env.HOME = fakeHome;
+
+    return electron.launch({
+      executablePath: '/usr/bin/session-desktop',
+    });
+  }
+  console.info(`   NON CI RUN`);
   console.info('   NODE_ENV', process.env.NODE_ENV);
   console.info('   NODE_APP_INSTANCE', process.env.NODE_APP_INSTANCE);
+
   const electronApp = await electron.launch({
     args: [join(getAppRootPath(), 'ts', 'mains', 'main_node.js')],
   });
