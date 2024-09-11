5e4f518fd16d1fa6c6dc3c458b3eeea8b65a808f
Audric Ackermann <audric@getsession.org>
2024-09-11 17:47:37 +1000

    chore: remove unused deps

--- tests/os_utils.ts
+++ tests/os_utils.ts
@@ -1,15 +1,2 @@
-import { release } from 'os';
-import { isUndefined } from 'lodash';
-import semver from 'semver';
-
 export const isMacOS = () => process.platform === 'darwin';
 export const isLinux = () => process.platform === 'linux';
-export const isWindows = (minVersion?: string) => {
-  const osRelease = release();
-
-  if (process.platform !== 'win32') {
-    return false;
-  }
-
-  return isUndefined(minVersion) ? true : semver.gte(osRelease, minVersion);
-};
